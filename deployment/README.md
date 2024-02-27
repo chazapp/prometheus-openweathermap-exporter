8b7db3631e2b4047f9a29b9e92ce26f5a797482f
chaz <shad@chaz.pro>
2024-02-27 19:30:02 +0100

    feat: Added terraform code deployment

---
+++ deployment/README.md
@@ -0,0 +1,48 @@
+# Deployment
+
+This directory contains the Terraform code to deploy the exporter and related tools to
+a Minikube cluster. It will install the following charts:
+
+- Kube-Prometheus-Stack
+- Grafana
+- Prometheus-Openweathermap-Exporter
+
+## Usage
+
+Running the Prometheus-Openweathermap-Exporter requires an OWM API key. To provide it, add a
+`secret.yaml` file to the Charts template:
+
+```yaml
+kind: Secret
+apiVersion: v1
+metadata:
+  name: prometheus-openweathermap-exporter
+stringData:
+  apiKey: <redacted>
+```
+
+Start a Minikube cluster, then apply the Terraform code:
+
+```bash
+$ minikube start
+...
+$ terraform apply
+...
+```
+
+Prometheus and Grafana have their Ingress enabled. Using `minikube tunnel` and editing your `/etc/hosts` file,
+you should be able to access them via browser
+
+```bash
+$ kubectl get ingress -n monitoring
+NAME                               CLASS   HOSTS              ADDRESS        PORTS   AGE
+grafana                            nginx   grafana.local      192.168.49.2   80      22s
+kube-prometheus-stack-prometheus   nginx   prometheus.local   192.168.49.2   80      41s
+
+# as root
+$ echo "192.168.42.2  grafana.local" >> /etc/hosts
+$ echo "192.168.42.2  prometheus.local" >> /etc/hosts
+$ minikube tunnel
+```
+
+On Windows you may need to use `127.0.0.1` instead in your hosts file.
---
+++ deployment/main.tf
@@ -0,0 +1,70 @@
+terraform {
+    required_providers {
+        kubernetes = {
+            source = "hashicorp/kubernetes"
+            version = "2.25.2"
+        }
+        helm = {
+            source = "hashicorp/helm"
+            version = "2.12.1"
+        }
+    }
+}
+
+provider "helm" {
+    kubernetes {
+        config_path = "~/.kube/config"
+    }
+}
+
+provider "kubernetes" {
+    config_path = "~/.kube/config"
+    config_context = "minikube"
+}
+
+resource "kubernetes_namespace" "tools_namespace" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
+resource "helm_release" "kube-prometheus-stack" {
+  name       = "kube-prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "56.7.0"
+  
+  namespace  = "monitoring"
+
+  values = [
+    "${file("${path.module}/kube-prometheus-stack.yaml")}"
+  ]
+}
+
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  repository = "https://grafana.github.io/helm-charts/"
+  chart      = "grafana"
+  version    = "7.3.2"
+
+  namespace  = "monitoring"
+
+  values = [
+    "${file("${path.module}/grafana.yaml")}"
+  ]
+  depends_on = [
+    helm_release.kube-prometheus-stack
+  ]
+}
+
+
+resource "helm_release" "prometheus-openweathermap-exporter" {
+  name      = "owm"
+  chart = "${path.module}/../charts/prometheus-openweathermap-exporter"
+  version = "1.0.0"
+  namespace = "monitoring"
+  depends_on = [
+    helm_release.kube-prometheus-stack
+  ]
+}
